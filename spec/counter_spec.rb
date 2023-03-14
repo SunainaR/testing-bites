1ee5767636355164bebb1b2b1fcbc5bf8e3f627a
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-03-14 18:47:29 +0000

    Added some more tests to counter spec after seeing the example solution

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -2,7 +2,7 @@ require 'counter'
 
 # I can know it's a class here in the spec file, because below uses the class rather than a string which is used for a method
 RSpec.describe Counter do
-  it "Shows me that I've counted to 13" do
+  it "Shows me that I've counted to 13 after adding one value" do
     counter = Counter.new
     counter.add(13)
     result = counter.report
@@ -10,14 +10,19 @@ RSpec.describe Counter do
   end
   it "Shows me that I've counted to 0" do
     counter = Counter.new
-    counter.add(0)
-    result = counter.report
-    expect(result).to eq "Counted to 0 so far."
+    expect(counter.report).to eq "Counted to 0 so far."
   end
-  it "Shows me that I've counted to 5.5" do
+  it "Shows me that I've counted to float 5.5" do
     counter = Counter.new
     counter.add(5.5)
     result = counter.report
     expect(result).to eq "Counted to 5.5 so far."
+  end
+  it "Shows me that I've counted 18 with multiple values added" do
+    counter = Counter.new
+    counter.add(10)
+    counter.add(3)
+    counter.add(5)
+    expect(counter.report).to eq "Counted to 18 so far."
   end
 end
